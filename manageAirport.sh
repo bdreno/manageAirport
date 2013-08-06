e2d402fe29dbf706b469dfb5366bb7524cf3acfb
Brad Reno <bd.reno+github@gmail.com>
2013-08-05 20:20:59 -0500

    JSS Compatability 1

--- manageAirport.sh
+++ manageAirport.sh
@@ -1,12 +1,43 @@
 #!/bin/sh
 
-#  serviceWiFiPrep.sh
-#  
+#  serviceWiFiPrep.sh <mountPoint> <computerName> <currentUsername> <Airport Power: On|Off> <Require Admin: On|Off> <Join Mode: Automatic|Preferred|Ranked|Recent|Strongest> <Preferred Network: Keep|Purge|Isolate> <Favorite Network: "Network SSID">
+#
+#  All fields are requried unless otherwise noted
+#   <mountPoint>    Reserved for the JSS
+#   <computerName>  Reserved for the JSS
+#   <currentUsername>   Reserved for the JSS
+#   <Airport Power: On|Off> Ensures the Network Adaptor is powered on or off
+#   <Require Admin: On|Off> Requires or disalbes admin access to change all wireless settings
+#   <Join Mode: Automatic|Preferred|Ranked|Recent|Strongest> Set the join moid
+#   <Preferred Network List: Keep|PurgeAll|Purge>  If Keep, leaves the Preferred Network list intact, PurgeAll Remove all saved netowrks, or Purge all but   
+#   <Favorite Network: "Network SSID">  Required only if "Preffered Network List" was set to Purge.  All networks not matching this SSID will be removed from the preferred network list.   
+#
 #
 #  Created by Bradley D. Reno on 7/23/13.
 #
-## Disable the remember networks feature
 
+#Define base variables
+airportPower=""
+requireAdmin=""
+joinMode=""
+preferredNetworkList=""
+favoriteNetwork=""
+
+
+if [ "$4" == "" && "$4" != "On" && "$4" != "on" && "$4" != "ON" && "$4" != "Off" && "$4" != "OFF" && "$4" != "off"]; then
+    echo "Usage Error <Airport Power: On|Off>"
+    exit 1
+else
+    airportPower="on"
+fi
+
+if [ "$5" == "" ]; then
+echo "Must supply a Password in variables 5"
+exit 1
+fi
+
+
+## Disable the remember networks feature
 ###set the desired preferences
 /System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport prefs JoinMode=Automatic RequireAdminNetworkChange=YES RememberRecentNetworks=YES RequireAdminPowerToggle=YES
 
